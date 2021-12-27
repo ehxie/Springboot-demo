e429f62780c780299dcdd6f090d86ecf6b493cb6
ehxie <815438732@qq.com>
2021-12-27 21:02:49 +0800

    mapper使用注解

--- src/main/java/com/ehxie/demo/mapper/UserMapper.java
+++ src/main/java/com/ehxie/demo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.ehxie.demo.mapper;
 
 import com.ehxie.demo.model.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface UserMapper {
      * 查询所有用户信息
      * @return 用户信息列表
      */
+    @Select("select * from user")
     List<User> selectAllUser();
 }
